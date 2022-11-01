65da29bf0bbde2136f8c603eea6e317b8fd561e6
Moises Navarro <moisalejandro@gmail.com>
2022-10-31 18:23:15 -0600

    [INIT]

---
+++ pos_order_line/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "PoS Order Line Tree View",
+    "version": "13.0.0.1.0",
+    "author": "Humanytek",
+    "license": "LGPL-3",
+    "depends": [
+        "point_of_sale",
+    ],
+    "data": [
+        # security
+        # data
+        # reports
+        # views
+        "views/pos_order_line.xml",
+    ],
+}
